5101799a590e77e2ccb009b276f39cc8ca3aa258
LotuSlime <92487717+Binglelicious@users.noreply.github.com>
2024-03-07 10:41:50 -0500

    trying to make the js and css load on the webpage

---
+++ index.js
@@ -0,0 +1,13 @@
+let calculation = localStorage.getItem('calculation') || '';
+
+document.querySelector('.js-display')
+  .innerHTML = `${calculation}`
+
+function updateCalculation (value) {
+  calculation += value;
+  
+  document.querySelector('.js-display')
+  .innerHTML = `${calculation}`
+
+  localStorage.setItem('calculation', calculation);
+}
